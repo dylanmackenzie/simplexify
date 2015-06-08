27ff8975fd37ff6a0d12f17f2a220106f9d485c6
Dylan MacKenzie <dylanxmackenzie@gmail.com>
2015-06-08 11:01:09 -0700

    Benchmark delaunay for powers of 2

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -3,48 +3,57 @@ import sr from 'seedrandom'
 import { slowPartition, partition } from './median'
 import Delaunay from './delaunay'
 
-const BENCH_LEN = 100
+const BENCH_LEN = 8
 
 let rng = new Math.seedrandom(42)
-let suite = new Benchmark.Suite()
+let suites = {
+  delaunay: new Benchmark.Suite('delaunay'),
+  partition: new Benchmark.Suite('partition'),
+  slowPartition: new Benchmark.Suite('slowPartition')
+}
 
 let benches = []
-for (let i = 0; i < BENCH_LEN; i++) {
+for (let i = 1; i < BENCH_LEN; i++) {
   let bench = []
-  for (let j = 0; j < i*71 + 4; j++) {
+  for (let j = 0; j < Math.pow(2, i); j++) {
     bench.push({ p: [rng(), rng()], t: null })
   }
   benches.push(bench)
 }
 
-function doBench(ar) {
-  let ar1 = Benchmark.deepClone(ar)
-  let ar2 = Benchmark.deepClone(ar)
-  let ar3 = Benchmark.deepClone(ar)
+benches.forEach(function(ar, i) {
+  suites.slowPartition.add('slowPartition\t'+i, function() {
+    slowPartition(Benchmark.deepClone(ar), 0, 0, ar.length-1)
+  })
 
-  // suite.add('slowPartition', function() {
-    // slowPartition(ar1, 0, 0, ar1.length-1)
-  // })
+  suites.partition.add('partition\t'+i, function() {
+    partition(Benchmark.deepClone(ar), 0, 0, ar.length-1)
+  })
 
-  // suite.add('partition', function() {
-    // partition(ar2, 0, 0, ar2.length-1)
-  // })
-
-  suite.add('delaunay', function () {
+  suites.delaunay.add('delaunay\t'+i, function () {
     let del = new Delaunay([])
-    del.verts = ar3
+    del.verts = Benchmark.deepClone(ar)
     del.delaunay()
   })
+})
+
+for (let name in suites) {
+  if (name !== 'delaunay') {
+    continue
+  }
+
+  suites[name].on('cycle', (e) => {
+    let index = e.target.name.split('\t')[1]
+    console.log(benches[index].length + '\t' + e.target.stats.mean)
+  })
+
+  suites[name].on('error', (e) => {
+    console.log(e.error)
+  })
+
+  suites[name].on('start', (suite) => {
+    console.log(suite.currentTarget.name + '\n')
+  })
+  suites[name].run()
 }
 
-benches.forEach(doBench)
-
-suite.on('cycle', (e) => {
-  console.log(benches[e.target.id-1].length + '\t' + e.target.stats.mean)
-})
-
-suite.on('error', (err) => {
-  console.log(err)
-})
-
-suite.run()
